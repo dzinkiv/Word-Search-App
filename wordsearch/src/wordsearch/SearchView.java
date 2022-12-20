21174d8648702720bf38f6e4daf87c5158e9c85e
David Zinkiv <david.zinkiv@gmail.com>
2022-12-20 17:03:32 -0500

    the inputed grid now converted to 2d char array

--- wordsearch/src/wordsearch/SearchView.java
+++ wordsearch/src/wordsearch/SearchView.java
@@ -1,5 +1,18 @@
 package wordsearch;
 
-public class SearchView {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+
+public class SearchView extends JFrame implements ActionListener{
+	public SearchView(char[][] data) {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
